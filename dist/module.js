19d829ee7bfe702f21e3048520e9cec851f13ed4
Svante Paldan <svantepaldan@spotify.com>
2016-05-30 15:43:14 +0200

    Initial commit

---
+++ dist/module.js
@@ -0,0 +1,22 @@
+'use strict';
+
+System.register(['./components/logs', './components/stream', './components/config'], function (_export, _context) {
+  var LogsPageCtrl, StreamPageCtrl, ExampleAppConfigCtrl;
+  return {
+    setters: [function (_componentsLogs) {
+      LogsPageCtrl = _componentsLogs.LogsPageCtrl;
+    }, function (_componentsStream) {
+      StreamPageCtrl = _componentsStream.StreamPageCtrl;
+    }, function (_componentsConfig) {
+      ExampleAppConfigCtrl = _componentsConfig.ExampleAppConfigCtrl;
+    }],
+    execute: function () {
+      _export('ConfigCtrl', ExampleAppConfigCtrl);
+
+      _export('StreamPageCtrl', StreamPageCtrl);
+
+      _export('LogsPageCtrl', LogsPageCtrl);
+    }
+  };
+});
+//# sourceMappingURL=module.js.map
